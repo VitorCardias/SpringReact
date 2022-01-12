a2aa19aad15499cc6fa1af17c1fef6862675592d
Vitor Serra Cardias <vitorserracardias2@outlook.com>
2022-01-12 18:24:51 -0300

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,6 @@
+function form(){
+    return(
+        <h1>Página de formulário</h1>
+    );
+}
+export default form;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,6 @@
+function listing(){
+    return(
+        <h1>Página de listagem</h1>
+    );
+}
+export default listing;
